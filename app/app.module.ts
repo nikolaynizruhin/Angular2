f25cc5daecd684ec673efe02a3d2e12a22e93233
nikolaynizruhin <lgnexus4fun@gmail.com>
2016-09-28 14:30:26 +0300

    add HTTP

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+import './rxjs-extensions';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
+import { HeroSearchComponent } from './hero-search.component';
 import { DashboardComponent } from './dashboard.component';
 import { HeroService } from './hero.service';
 import { routing } from './app.routing';
@@ -13,13 +19,16 @@ import { routing } from './app.routing';
   imports: [
       BrowserModule,
       FormsModule,
+      HttpModule,
+      InMemoryWebApiModule.forRoot(InMemoryDataService),
       routing
   ],
   declarations: [
       AppComponent,
       HeroDetailComponent,
       HeroesComponent,
-      DashboardComponent
+      DashboardComponent,
+      HeroSearchComponent
   ],
   providers: [
     HeroService
